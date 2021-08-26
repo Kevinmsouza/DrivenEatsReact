bd5c5e908b0df5af16aeca29f134a64a6aa33ccb
Kevinmsouza <kevinm.souza@hotmail.com>
2021-08-26 15:27:56 -0400

    Estruturada divisão do componente App

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <div class="footer">
+            <button class="botao" disabled>
+                Selecione os 3 itens
+                para fechar o pedido
+            </button>
+        </div>
+    )
+}
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div class="header">
+            <div>
+                <h1 class="title">DrivenEats</h1>
+                <p>Sua comida em 6 minutos</p>
+            </div>
+        </div>
+    )
+}
